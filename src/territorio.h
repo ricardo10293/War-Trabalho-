466d8f1cd9c0b8881100d9a8e6f1bd9076bfbbb8
ricardo10293 <ricardosoares11309@gmail.com>
2025-11-05 11:40:38 -0300

    Update territorio.h

--- src/territorio.h
+++ src/territorio.h
@@ -1,6 +1,8 @@
 #ifndef TERRITORIO_H
 #define TERRITORIO_H
 
+#include "jogador.h"  // Adicionando a inclusão do arquivo jogador.h
+
 void distribuirTerritorios(Jogador* jogadores, int numJogadores);
 
 #endif
