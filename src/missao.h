d3a56a5a04587655599e62005cb5b3a12ae3ac6f
ricardo10293 <ricardosoares11309@gmail.com>
2025-11-05 11:37:56 -0300

    Update missao.h

--- src/missao.h
+++ src/missao.h
@@ -1,12 +1,6 @@
 #ifndef MISSAO_H
 #define MISSAO_H
 
-typedef struct {
-    char descricao[100];
-    int tipo; // 1 = conquistar territórios, 2 = eliminar jogador, etc.
-    int objetivo;
-} Missao;
-
-int verificarMissao(int numTerritorios, Missao* missao);
+int verificarVitoria(Jogador* jogadores, int numJogadores);
 
 #endif
