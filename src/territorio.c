5c6f2b1909f1904e1f7a3447709c5dcc5f201c6c
ricardo10293 <ricardosoares11309@gmail.com>
2025-11-05 11:36:54 -0300

    Update territorio.c

--- src/territorio.c
+++ src/territorio.c
@@ -1,16 +1,19 @@
+#include "territorio.h"
+#include "jogador.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "territorio.h"
 
-Territorio* criarTerritorio(char* nome, int tropas) {
-    Territorio* t = (Territorio*) malloc(sizeof(Territorio));
-    strncpy(t->nome, nome, MAX_NOME);
-    t->tropas = tropas;
-    t->dono = -1; // Inicialmente, sem dono
-    return t;
-}
+void distribuirTerritorios(Jogador* jogadores, int numJogadores) {
+    for (int i = 0; i < numJogadores; i++) {
+        jogadores[i].numTerritorios = 3;  // Cada jogador começa com 3 territórios
+        jogadores[i].territorios = malloc(jogadores[i].numTerritorios * sizeof(char*));
 
-void liberarTerritorio(Territorio* territorio) {
-    free(territorio);
+        // Aloca e distribui os territórios (simulação)
+        for (int j = 0; j < jogadores[i].numTerritorios; j++) {
+            jogadores[i].territorios[j] = malloc(30 * sizeof(char));  // Nome do território
+            sprintf(jogadores[i].territorios[j], "Território %d", j + 1);  // Nome genérico do território
+        }
+
+        printf("Jogador %s recebeu %d territórios.\n", jogadores[i].nome, jogadores[i].numTerritorios);
+    }
 }
