f79d4207b07377c54aa8b24ee0411333910f5a50
ricardo10293 <ricardosoares11309@gmail.com>
2025-11-05 11:36:36 -0300

    Update territorio.h

--- src/territorio.h
+++ src/territorio.h
@@ -1,15 +1,6 @@
 #ifndef TERRITORIO_H
 #define TERRITORIO_H
 
-#define MAX_NOME 50
-
-typedef struct {
-    char nome[MAX_NOME];
-    int tropas;
-    int dono; // índice do jogador dono do território
-} Territorio;
-
-Territorio* criarTerritorio(char* nome, int tropas);
-void liberarTerritorio(Territorio* territorio);
+void distribuirTerritorios(Jogador* jogadores, int numJogadores);
 
 #endif
