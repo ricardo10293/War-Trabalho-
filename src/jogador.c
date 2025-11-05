a322c04c3dd085d7c19752e0f4559a3fe937df2e
ricardo10293 <ricardosoares11309@gmail.com>
2025-11-05 09:32:48 -0300

    jogador.c

--- src/jogador.c
+++ src/jogador.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "jogador.h"
 
+Jogador* cadastrarJogadores(int n) {
+    Jogador* jogadores = (Jogador*) malloc(n * sizeof(Jogador));
+    for (int i = 0; i < n; i++) {
+        printf("\n--- Cadastro do Jogador %d ---\n", i + 1);
+        printf("Nome: ");
+        scanf(" %[^\n]", jogadores[i].nome);
+        printf("Cor: ");
+        scanf(" %[^\n]", jogadores[i].cor);
+        jogadores[i].territorios = NULL;
+        jogadores[i].numTerritorios = 0;
+    }
+    return jogadores;
+}
+
+void liberarJogadores(Jogador* jogadores, int n) {
+    for (int i = 0; i < n; i++) {
+        free(jogadores[i].territorios);
+    }
+    free(jogadores);
+}
