da997adcea4795515ee7197361e9f1d3fd19d31b
ricardo10293 <ricardosoares11309@gmail.com>
2025-11-05 11:02:11 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,19 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
+// src/main.c
 #include "jogador.h"
-#include "territorio.h"
-#include "batalha.h"
-#include "missao.h"
+#include <stdio.h>
+
+// Declarar as funções que você vai usar
+Jogador* cadastrarJogadores(int numJogadores);
+void liberarJogadores(Jogador* jogadores, int numJogadores);
 
 int main() {
-    int numJogadores;
-    printf("Quantos jogadores? ");
-    scanf("%d", &numJogadores);
-
+    int numJogadores = 2;
     Jogador* jogadores = cadastrarJogadores(numJogadores);
-
-    // Simulando o jogo
-    // [Adicione o loop principal aqui com interações de ataques, missões etc.]
+    
+    // Lógica do jogo...
 
     liberarJogadores(jogadores, numJogadores);
     return 0;
